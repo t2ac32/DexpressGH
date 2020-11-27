ef400ba1d70864a90b7a30f1de766c399fdefc6b
Tracer <eduardo.pradoruiz@gmail.com>
2020-11-27 20:24:41 +0100

    first commit
    Created project

---
+++ DexpressGHTests/DexpressGHTests.swift
@@ -0,0 +1,34 @@
+//
+//  DexpressGHTests.swift
+//  DexpressGHTests
+//
+//  Created by Tracer on 27/11/20.
+//  Copyright © 2020 Tracer. All rights reserved.
+//
+
+import XCTest
+@testable import DexpressGH
+
+class DexpressGHTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
