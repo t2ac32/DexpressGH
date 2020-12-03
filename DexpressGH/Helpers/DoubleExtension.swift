d641ce0376a5815c3c1f7e384fd583ed8a692a7d
Tracer <eduardo.pradoruiz@gmail.com>
2020-12-03 22:43:18 +0100

    fix stats labels format and size

--- DexpressGH/Helpers/DoubleExtension.swift
+++ DexpressGH/Helpers/DoubleExtension.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension Double {
     func unitFormatted() -> String {
-        if self >= 10000, self <= 999999 {
+        if self >= 1000, self <= 999999 {
             return String(format: "%.1fk", locale: Locale.current, self/1000).replacingOccurrences(of: ".0", with: "")
         }
         if self >= 999999 {
--- DexpressGH/Modules/MainModule/cell/RepoItemCell.swift
+++ DexpressGH/Modules/MainModule/cell/RepoItemCell.swift
@@ -16,6 +16,11 @@ class RepoItemCell: UITableViewCell {
     @IBOutlet weak var forksLabel: UILabel!
     @IBOutlet weak var watchersLabel: UILabel!
     @IBOutlet weak var followersLabel: UILabel!
+    
+    @IBOutlet weak var watchersWidth: NSLayoutConstraint!
+    @IBOutlet weak var followersWidth: NSLayoutConstraint!
+    @IBOutlet weak var forksWidth: NSLayoutConstraint!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -29,32 +34,34 @@ class RepoItemCell: UITableViewCell {
         configAvatar(imageView: avatarImageView, url: repoModel.avatarUrl)
         titleLabel.text = repoModel.title
         descriptionLabel.text = repoModel.description
-        var constraints: [NSLayoutConstraint] = []
+        followersLabel.translatesAutoresizingMaskIntoConstraints = false
+        watchersLabel.translatesAutoresizingMaskIntoConstraints = false
+        forksLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         if let followers = repoModel.followers {
             followersLabel.text = followers
             showView(hide: false, view: followersLabel)
-            constraints.append(followersLabel.widthAnchor.constraint(equalToConstant: 27.0))
+            followersWidth.constant = 30.0
         } else {
-            constraints.append(followersLabel.widthAnchor.constraint(equalToConstant: 0.0))
+            followersWidth.constant = 0.0
             showView(hide: true, view: followersLabel)
         }
         if let watchers = repoModel.watchers {
             watchersLabel.text = watchers
             showView(hide: false, view: watchersLabel)
-            constraints.append(watchersLabel.widthAnchor.constraint(equalToConstant: 27.0))
+            watchersWidth.constant = 30.0
         } else {
-            constraints.append(watchersLabel.widthAnchor.constraint(equalToConstant: 0.0))
+            watchersWidth.constant = 0.0
             showView(hide: true, view: watchersLabel)
         }
         if let forks = repoModel.forks {
             forksLabel.text = forks
             showView(hide: false, view: followersLabel)
-            constraints.append(forksLabel.widthAnchor.constraint(equalToConstant: 27.0))
+            forksWidth.constant = 30.0
         } else {
-            constraints.append(forksLabel.widthAnchor.constraint(equalToConstant: 0.0))
+            forksWidth.constant = 0.0
             showView(hide: true, view: followersLabel)
         }
-        NSLayoutConstraint.activate(constraints)
         self.layoutIfNeeded()
     }
     func configAvatar(imageView: UIImageView, url: String) {
